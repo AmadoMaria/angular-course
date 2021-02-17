8a341402143b882d25cafcadac7975302eb8d2bd
AmadoMaria <mariantoniamado@hotmail.com>
2021-02-17 20:30:14 -0300

    create card component

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input()
+  course: Course
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCourseViewed(){
+    console.log("click")
+
+    this.courseEmitter.emit(this.course)
+  }
+
+}
